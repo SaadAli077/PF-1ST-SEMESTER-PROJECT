3c4817549bb6711ce74669ed3533c23ff3d8af5e
hassanmirza28 <mirzahasan201@gmail.com>
2025-01-26 18:16:48 +0500

    done by hassan

--- final_project.cpp
+++ final_project.cpp
@@ -5,3 +5,38 @@
 
 using namespace std;
 
+struct Flight {
+int flightNumber;
+string destination;
+int economySeatsAvailable;
+int businessSeatsAvailable;
+string departureTime;
+string arrivalTime;
+};
+struct Passenger {
+string name;
+int flightNumber;
+string bookingDate;
+string phoneNumber;
+int age;
+string flyingClass;
+};
+void viewBookingRecords() {
+ifstream file("bookings.txt");
+if (!file) {
+cout << "No booking records found.\n";
+return;
+}
+cout << "\nBooking Records:\n";
+cout << "Flight Number Name Booking Date Phone Number Age Flying
+Class\n";
+cout << "--------------------------------------------------------------------------------------------\n";
+Passenger p;
+while (file >> p.flightNumber >> p.name >> p.bookingDate >> p.phoneNumber >> p.age >>
+p.flyingClass) {
+cout << p.flightNumber << " " << p.name << " "
+<< p.bookingDate << " " << p.phoneNumber << " "
+<< p.age << " " << p.flyingClass << "\n";
+}
+file.close();
+}
